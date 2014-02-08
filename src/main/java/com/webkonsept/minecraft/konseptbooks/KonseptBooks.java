5e314abafb1a4eac73b75414875bb81fd5f1d1ac
Fredrik Vold <fredrik@webkonsept.com>
2014-02-08 23:52:43 +0100

    Configuration! Yay!
    Implemented configuration, and set handing out newbie books to
    false by default.

--- src/main/java/com/webkonsept/minecraft/konseptbooks/KonseptBooks.java
+++ src/main/java/com/webkonsept/minecraft/konseptbooks/KonseptBooks.java
@@ -5,18 +5,21 @@ import com.webkonsept.minecraft.konseptbooks.storage.KonseptBook;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Iterator;
 
 public class KonseptBooks extends JavaPlugin {
 
     private KonseptBooksLibrary library;
+
+    // FIXME Why are these even here?
     private KonseptBooksListener eventListener;
     private Executor commandExecutor;
 
-    // TODO - Make this stuff configurable!
+    // TODONE - Made this stuff configurable!
     protected boolean autosave = true;
-    protected boolean giveNewbieBook = true;
+    protected boolean giveNewbieBook = false;
     protected String newbieBookName = "Welcome";
 
     @Override
@@ -27,15 +30,24 @@ public class KonseptBooks extends JavaPlugin {
         commandExecutor = new Executor(this);
 
         getServer().getPluginManager().registerEvents(eventListener,this);
+
+        loadConfiguration();
     }
     @Override
     public void onDisable() {
-        library.save();
+        // library.save(); // If autosave is on, it's already saved.  If it's off, we shouldn't save without prompting.
     }
 
+    /**
+     * Load The Configuration!
+     */
     public void loadConfiguration(){
-
+        saveDefaultConfig();
+        autosave = getConfig().getBoolean("autosave",true);
+        giveNewbieBook = getConfig().getBoolean("giveNewbieBook",false);
+        newbieBookName = getConfig().getString("newbieBookName","Welcome");
     }
+
     /**
      * Get a hold of the library associated with this plugin instance
      * @return The currently ruling library
@@ -48,17 +60,21 @@ public class KonseptBooks extends JavaPlugin {
      * Get a hold of the event listener associated with this plugin instance
      * @return The registered event listener
      */
+    /* UNUSED
     public KonseptBooksListener getEventListener(){
         return eventListener;
     }
+    */
 
     /**
      * Get your grubby mitts on the command executor this plugin instance uses.
      * @return The command executor that executes commands for this plugin instance.
      */
+    /* UNUSED
     public Executor getCommandExecutor(){
         return commandExecutor;
     }
+    */
 
     /**
      * Join up an array of strings.
@@ -78,6 +94,7 @@ public class KonseptBooks extends JavaPlugin {
         }
         return sb.toString();
     }
+
     /**
      * Takes a book title, or any string for that matter, and turns it into a standardized form used to store book names in YAML.
      * @param title The string you want standardized
